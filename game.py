e28e52cebc622bca71a392f82ada1fbdac16c325
gabriel-ruffo <gabriel.ruffo@outlook.com>
2020-01-16 22:15:04 -0700

    feat: add discard function
    Added beginnings of discard function. Works so far by disregarding the
    important cards from the hand to look for a discard candidate.

--- game.py
+++ game.py
@@ -1,14 +1,19 @@
 from deck import Deck
 from player import Player
+from collections import Counter
 
 class Game:
     def __init__(self, players):
         self.deck = Deck()
+        self.discard_pile = Deck()
         self.players = players
         self.current_round = 6
 
     def setup_next_round(self):
+        # reset the deck
         self.deck.reinitialize()
+        # empty the discard pile
+        self.discard_pile = Deck()
         self.clear_player_hands()
 
         self.deal(self.current_round)
@@ -22,29 +27,57 @@ class Game:
             player.set_downed_hand(None)
 
 
-    def check_win_conditions(self, deck):
+    def check_win_conditions(self, hand):
         if self.current_round == 6:
             # two tercias
-            return
+            tercias, _ = hand.find_tercias()
+            if len(tercias) >= 2:
+                print(tercias)
+                return True
+            return False
         elif self.current_round == 7:
             # one tercia, one run
-            return
+            return False
         elif self.current_round == 8:
             # two runs
-            return
+            return False
         elif self.current_round == 9:
             # three tercias
-            return
+            return False
         elif self.current_round == 10:
             # two tercias, one run
-            return
+            return False
         elif self.current_round == 11:
             # one tercia, two runs
-            return
+            return False
         elif self.current_round == 12:
             # four tercias
-            return
+            return False
         # second 12: three runs, no discard
+
+    def get_unnecessary_cards(self, hand):
+        tercias, possibles = hand.find_tercias()
+        result = Deck()
+
+        print("HAND     :", hand.deck_to_string())
+        print("TERCIAS  :", tercias)
+        print("POSSIBLES:", possibles)
+
+        for card in hand.my_deck:
+            if card.get_value() in tercias or card.get_value() in possibles:
+                continue
+            else:
+                result.add(card)
+
+        print("NEW HAND :", result.deck_to_string())
+        return result
+
+    def discard(self, hand):        
+        hand_copy = self.get_unnecessary_cards(hand)
+        
+        print("\n\n")
+        return False
+
 
     def play(self):
         for player in self.players:
@@ -55,7 +88,10 @@ class Game:
             # sort the current hand
             hand_in_play.deck_selection_sort()
 
-            self.check_win_conditions(hand_in_play)
+            if self.check_win_conditions(hand_in_play):
+                print("Implement go_down()")
+
+            self.discard(hand_in_play)
 
             
     def deal(self, round):
