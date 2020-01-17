e0c07421a42ea7174feb4c09b7516ab7a2cca77a
gabriel-ruffo <gabriel.ruffo@outlook.com>
2020-01-16 18:50:25 -0700

    feat: start check_win_conditions
    Started implementation of check_win_conditions.
    Using if/elif statements to determine what the game
    should be checking for based on what round it is.

--- game.py
+++ game.py
@@ -12,22 +12,52 @@ class Game:
         self.clear_player_hands()
 
         self.deal(self.current_round)
-        self.current_round += 1
+        # might have to change this incr to after the round
+        # is over
         self.play()
+        self.current_round += 1
 
     def clear_player_hands(self):
         for player in self.players:
             player.set_downed_hand(None)
 
+
+    def check_win_conditions(self, deck):
+        if self.current_round == 6:
+            # two tercias
+            return
+        elif self.current_round == 7:
+            # one tercia, one run
+            return
+        elif self.current_round == 8:
+            # two runs
+            return
+        elif self.current_round == 9:
+            # three tercias
+            return
+        elif self.current_round == 10:
+            # two tercias, one run
+            return
+        elif self.current_round == 11:
+            # one tercia, two runs
+            return
+        elif self.current_round == 12:
+            # four tercias
+            return
+        # second 12: three runs, no discard
+
     def play(self):
         for player in self.players:
-            player.get_hand_in_play().deck_selection_sort()
-            print(player.get_hand_in_play().deck_to_string())
-            tercias, possibles = player.get_hand_in_play().find_tercias()
-            print("\nfor player {}:".format(self.players.index(player)))
-            print(tercias)
-            print(possibles)
+            hand_in_play = player.get_hand_in_play()
+            # draw a card
+            hand_in_play.add(self.deck.pop())
 
+            # sort the current hand
+            hand_in_play.deck_selection_sort()
+
+            self.check_win_conditions(hand_in_play)
+
+            
     def deal(self, round):
         # for each player in the game
         for player in self.players:
@@ -38,3 +68,13 @@ class Game:
             # give player random hand
             player.set_hand_in_play(temp_hand)
             player.add_to_beginning_hands(temp_hand)
+
+
+player1 = Player(None, [], [], None)
+player2 = Player(None, [], [], None)
+players = []
+players.append(player1)
+players.append(player2)
+
+game = Game(players)
+game.setup_next_round()
